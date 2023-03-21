a3364a5fc2f526217008d7d5bee1ddae42258f9e
Чепурной Александр Владимирович <chepurnoy.a.v@sberbank.ru>
2023-03-21 13:19:00 +0300

    upate

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,2 @@
 export declare const add: (a: number, b: number) => number;
+export * from './components';
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export const add = (a: number, b: number): number => a + b;
+
+export * from './components'
